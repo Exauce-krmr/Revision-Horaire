a5703b3be34347da2c3ceb5edaf2f6f5bf15d446
Exo-create <exokrameronex@gmail.com>
2026-09-17 08:52:27 +0200

    Add config

---
+++ config/constants.php
@@ -0,0 +1,21 @@
+<?php
+
+define("KEY_MESSAGE" , "message");
+define("KEY_FIELD" , "field");
+define("KEY_ID" , "id");
+
+define("KEY_COURSE_NAME" , "courseName");
+define("KEY_CLASS_NAME" , "className");
+//define("KEY_COURSE_NAME" , "courseName");
+//define("KEY_CLASS_NAME" , "className");
+
+define("KEY_VALUE" , "value");
+
+enum Days : string
+{
+    case Lundi = 'lundi';
+    case Mardi = 'mardi';
+    case Mercredi = 'mercredi';
+    case Jeudi = 'jeudi';
+    case Vendredi = 'vendredi';
+}
---
+++ config/database.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types=1);
+define('DB_HOST', 'db');
+
+define('DB_NAME', 'horaire_eleve');
+define('DB_USER', 'me');
+define('DB_PASSWORD', '1234');
