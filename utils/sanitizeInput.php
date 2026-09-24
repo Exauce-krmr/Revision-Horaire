4a9d2df34a6171d27726980d4699bc2ca4019d6f
Exo-create <exokrameronex@gmail.com>
2026-09-24 08:42:50 +0200

    modify utils

--- utils/sanitizeInput.php
+++ utils/sanitizeInput.php
@@ -13,6 +13,6 @@ function sanitizeInput($value, Type $type)
     return match ($type) {
         Type::email => filter_var($value, FILTER_SANITIZE_EMAIL),
         Type::int => filter_var($value, FILTER_SANITIZE_NUMBER_INT),
-        Type::string => filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+        Type::string => htmlspecialchars($value, ENT_QUOTES, "UTF-8"),
     };
 }
